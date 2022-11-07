d8ae1a74387ade043c78b11e008c4048497324f0
STIg89 <andreystig89@gmail.com>
2022-11-07 15:25:28 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,30 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const galleryItemsMarkup = createGallery(galleryItems);
+
+function createGallery(items) {
+  return items
+    .map(
+      item =>
+        `
+          <a class="gallery__item" href="${item.original}">
+            <img
+              class="gallery__image"
+              src="${item.preview}"
+              alt="${item.description}"
+            />
+          </a>
+          `
+    )
+    .join('');
+}
+
+gallery.innerHTML = galleryItemsMarkup;
+
+const lightbox = new SimpleLightbox('.gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
