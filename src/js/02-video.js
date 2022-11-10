800289211fb05a8a0d1eb5cbf626b9164fec39c8
STIg89 <andreystig89@gmail.com>
2022-11-10 20:33:14 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,15 @@
-import Player from '@vimeo/player';
-
-const player = new Player('handstick', {
-  id: 19231868,
-  width: 640,
-});
-
-player.on('play', function () {
-  console.log('played the video!');
-});
+// import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
+//
+// const player = new Player('handstick', {
+//   id: 19231868,
+//   width: 640,
+// });
+//
+// player.on('timeupdate', throttle(onPlay, 1000));
+//
+// const onPlay = function ({ seconds }) {
+//   localStorage.setItem('videoplayer-current-time', seconds);
+// };
+//
+// player.setCurrentTime(localStorage.setItem('videoplayer-current-time'));
