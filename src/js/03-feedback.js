b6a1f3a212646ea73b040b14dab715659ce5f9fe
STIg89 <andreystig89@gmail.com>
2022-11-11 22:54:32 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('.feedback-form input'),
+  message: document.querySelector('.feedback-form textarea'),
+};
+
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+refs.form.addEventListener('input', throttle(onInput, 500));
+refs.form.addEventListener('submit', onSubmit);
+
+localStorageData();
+
+function onInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+
+function localStorageData() {
+  const data = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (data) {
+    refs.email.value = data.email;
+    refs.message.value = data.message;
+  }
+}
