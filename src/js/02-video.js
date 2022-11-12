cc1ee62ed10533f9e65da48cbb96f43689605ab6
STIg89 <andreystig89@gmail.com>
2022-11-12 12:20:25 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,11 +3,14 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay({ seconds }) {
-  localStorage.setItem('videoplayer-current-time', seconds);
+  localStorage.setItem(STORAGE_KEY, seconds);
 }
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+if (localStorage.getItem(STORAGE_KEY)) {
+  player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
+}
